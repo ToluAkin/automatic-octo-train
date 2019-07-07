c68bd4d9c6ad83cf3f4717dace54edebbeb35913
Tolu Akin <toluakintoye@gmail.com>
2019-07-07 18:36:12 +0100

    baseline

---
+++ common/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <p>CONTACT US</p>
+    <ul class="list-inline">
+        <li class="list-inline-item"><a href="www.facebook.com"><i class="fab fa-facebook-f"></i></a></li>
+        <li class="list-inline-item"><a href="www.twitter.com"><i class="fab fa-twitter"></i></a></li>
+        <li class="list-inline-item"><a href="www.linkedin.com"><i class="fab fa-linkedin-in"></i></a></li>
+    </ul>
+    <p>All rights reserved. &copy; 2018 Green Scrapbook</p>
+</footer>
---
+++ common/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Colored Lists |<?php echo $pageTitle ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div id="page-wrap">
+        <div id="header">
+            <h1><a href="/">Colored Lists</a></h1>
+            <div id="control">
+                <!-- IF LOGGED IN -->
+                <p><a href="/logout.php" class="button">Log out</a> <a href="account.php" class="button">Your Account</a></p>
+                <!-- IF LOGGED OUT -->
+                <p><a class="button" href="/signup.php">Sign up</a> &nbsp; <a class="button" href="login.php">Log in</a></p>
+                <!-- END OF IF STATEMENT -->
+            </div>
+        </div>
+    </div>
+    <footer></footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src='main.js'></script>
+</body>
+</html>
